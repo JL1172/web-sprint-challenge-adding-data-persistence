e7d703cca0dd07fa430d5ffcb3f1684455bc4940
Jacob Lang <jacoblang72@comcast.net>
2023-10-11 15:24:14 -0400

    finished project router i think

--- api/project/middleware.js
+++ api/project/middleware.js
@@ -1,1 +1,23 @@
-const db = require("../../data/dbConfig");
+
+
+module.exports = {
+    validatePostBody,
+}
+
+async function validatePostBody(req,res,next) {
+    try {
+        const {project_name} = req.body;
+        if (!project_name || typeof project_name !== "string") {
+            next({status : 422, message : "project name must be string and is required"})
+        } else {
+            if (req.body.project_description && typeof req.body.project_description !== "string") {
+                next({status : 422, message : "project description must be string"})
+            } else if (req.body.project_completed && 
+                typeof req.body.project_completed !== "boolean") {
+                    next({status : 422, message : "must be a boolean value"})
+                } else {
+                    next(); 
+                }
+        }
+    } catch (err) {next(err)}
+}
--- api/project/model.js
+++ api/project/model.js
@@ -1,2 +1,17 @@
 // build your `Project` model here
 const db = require("../../data/dbConfig")
+
+module.exports = {
+    findAll,
+    create
+}
+
+async function findAll() {
+    const projectData = await db("projects");
+    return projectData;
+}
+
+async function create(createdProject) {
+    const created = await db("projects").insert(createdProject);
+    return await db("projects").where("project_id",created[0]).first(); 
+}
