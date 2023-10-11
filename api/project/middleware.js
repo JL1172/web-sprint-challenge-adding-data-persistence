bbe76e05776cb08c2fcb62ca3147476605aa46f3
Jacob Lang <jacoblang72@comcast.net>
2023-10-11 15:44:03 -0400

    finished projects endpoint

--- api/project/middleware.js
+++ api/project/middleware.js
@@ -4,20 +4,17 @@ module.exports = {
     validatePostBody,
 }
 
-async function validatePostBody(req,res,next) {
+async function validatePostBody(req, res, next) {
     try {
-        const {project_name} = req.body;
+        const { project_name, project_description } = req.body;
         if (!project_name || typeof project_name !== "string") {
-            next({status : 422, message : "project name must be string and is required"})
+                next({status : 422,message : "need project name"})
         } else {
-            if (req.body.project_description && typeof req.body.project_description !== "string") {
-                next({status : 422, message : "project description must be string"})
-            } else if (req.body.project_completed && 
-                typeof req.body.project_completed !== "boolean") {
-                    next({status : 422, message : "must be a boolean value"})
-                } else {
-                    next(); 
-                }
+            if (project_description && typeof project_description !== "string") {
+                next({status : 422,message : "project description must be string"})
+            } else {
+                next();
+            }
         }
-    } catch (err) {next(err)}
+    } catch (err) { next(err) }
 }
--- api/project/model.js
+++ api/project/model.js
@@ -8,10 +8,29 @@ module.exports = {
 
 async function findAll() {
     const projectData = await db("projects");
-    return projectData;
+    const returnValue = projectData.map(n=> {
+        if (!n.project_completed) {
+            return {...n, project_completed : false}
+        } else if (n.project_completed) {
+            return {...n, project_completed : true}
+        } else {
+            return n;
+        }
+    })
+    return returnValue;
 }
 
 async function create(createdProject) {
     const created = await db("projects").insert(createdProject);
-    return await db("projects").where("project_id",created[0]).first(); 
+    const inter = await db("projects").where("project_id",created[0]); 
+    const returnValue = inter.map(n=> {
+        if (!n.project_completed) {
+            return {...n, project_completed : false}
+        } else if (n.project_completed) {
+            return {...n, project_completed : true}
+        } else {
+            return n;
+        }
+    })
+    return returnValue[0];
 }
