1497f2c3d63e58e9e23a9a55bb124015ec5fe6bf
Jacob Lang <jacoblang72@comcast.net>
2023-10-11 13:41:51 -0400

    added routers fleshed out server and router

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,14 @@
 // build your `/api/resources` router here
+const express = require('express');
+
+
+const router = express.Router();
+
+router.use((error,req,res,next)=> { //eslint-disable-line
+    res.status(error.status || 500).json({
+        message : error.message,
+        customMessage : "You messed up lol",
+        stack : error.stack,
+    })
+})
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+const express = require("express");
+const TaskRouter = require("./task/router");
+const ResourceRouter = require("./resource/router");
+const ProjectRouter = require("./project/router")
+const morgan = require("morgan")
+
+
+const server = express();
+
+server.use(express.json())
+server.use(morgan("dev")); 
+
+//routes
+server.use("/api/project",ProjectRouter);
+server.use("/api/resource",ResourceRouter);
+server.use("/api/task",TaskRouter);
+//routes
+
+module.exports = server; 
